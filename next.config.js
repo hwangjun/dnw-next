8b6ab2cf2ad86c0490e19e8003a5ae3f14964ee8
issac-oh <issac1162@gmail.com>
2023-02-20 20:19:57 +0900

    build error 수정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  /* config options here */
 };
 
-export default nextConfig;
+module.exports = nextConfig;
